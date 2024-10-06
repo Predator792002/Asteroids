c66a50174f5478e112d76265cd4ca8064636ea7d
Ayush Gupta <90502624+Predator792002@users.noreply.github.com>
2024-10-06 21:58:56 +0530

    Update game logic and add player functionality
    - Modified constants.py to include new game settings.
    - Updated main.py for improved game loop and initialization.
    - Added player.py to define player attributes and behaviors.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -13,10 +16,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))  
+        player.draw(screen)
         pygame.display.flip()
-        dt = pygame.time.Clock.tick(60) / 1000.0
+        dt = clock.tick(60) / 1000
+        
 
 
 
 if __name__ == "__main__":
     main()
+
