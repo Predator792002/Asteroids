d8341a6ddd9d1e4a61d264e22d0086ae15e8e428
Ayush Gupta <90502624+Predator792002@users.noreply.github.com>
2025-02-15 11:19:35 +0530

    Modified main.py to remove unnecessary veriable

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
-    asteroid_field = AsteroidField()
+    AsteroidField()
 
     Player.containers = (updatable, drawable)
 
